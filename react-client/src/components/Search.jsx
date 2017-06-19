9224017535c734489ea2445742b2512168b2491a
ststar8623 <ststar8623@gmail.com>
2017-06-19 16:49:05 -0700

    search button working

---
+++ react-client/src/components/Search.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artist: ''
+    }
+  };
+
+  handleChange(e) {
+    console.log('search side', e.target.value);
+    this.setState({
+      artist: e.target.value
+    });
+  }
+
+  search() {
+    this.props.handleChange(this.state.artist);
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange.bind(this)} ></input>
+        <input type="submit" onClick={this.search.bind(this)}></input>
+      </div>
+    )
+  }
+}
+
+export default Search;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import Search from './components/Search.jsx';
 import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      items: []
-    }
+      items: [],
+      artist: ''
+    };
+
+    this.search = this.search.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     $.ajax({
       url: '/items', 
+      method: 'GET',
       success: (data) => {
         this.setState({
           items: data
@@ -25,10 +31,28 @@ class App extends React.Component {
     });
   }
 
+  handleChange(e) {
+    console.log('name: ', e)
+    this.setState({
+      artist: e
+    });
+  }
+
+  search() {
+    $.ajax({
+      url: '',
+      method: 'POST',
+      success: (data) => {
+        console.log(data + ' successful sent!!');
+      }
+    })
+  }
+  
   render () {
     return (<div>
       <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <Search handleChange={this.handleChange} />
+      <List />
     </div>)
   }
 }
